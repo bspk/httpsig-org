fd18649280a2f7b6525f4e86cefdd31e56b72ebd
Justin Richer <justin@bspk.io>
2024-03-22 16:13:30 +1000

    add additional libraries and tweak display

--- frontend/src/components/libraries.js
+++ frontend/src/components/libraries.js
@@ -3,21 +3,27 @@ const libraryList = [
     language: 'Java',
     maintainer: 'Bespoke Engineering',
     maintainerLink: 'https://bspk.io/',
-    repo: 'https://github.com/bspk/httpsig-java/',
+    repo: 'https://github.com/bspk/httpsig-java',
+    request: true,
+    response: true,
     sign: true,
     verify: true
   },
   {
     language: 'Go',
     maintainer: 'Yaron Sheffer',
-    repo: 'https://github.com/yaronf/httpsign/',
+    repo: 'https://github.com/yaronf/httpsign',
+    request: true,
+    response: true,
     sign: true,
     verify: true
   },
   {
     language: 'Scala',
     maintainer: 'Henry Story',
-    repo: 'https://github.com/bblfish/httpSig/',
+    repo: 'https://github.com/bblfish/httpSig',
+    request: true,
+    response: true,
     sign: true,
     verify: true
   },
@@ -25,7 +31,9 @@ const libraryList = [
     language: 'Python',
     maintainer: 'Bespoke Engineering',
     maintainerLink: 'https://bspk.io/',
-    repo: 'https://github.com/bspk/httpsigpy/',
+    repo: 'https://github.com/bspk/httpsigpy',
+    request: true,
+    response: true,
     sign: true,
     verify: true
   },
@@ -33,6 +41,36 @@ const libraryList = [
     language: 'Python',
     maintainer: 'Andrey Kislyuk',
     repo: 'https://github.com/pyauth/http-message-signatures',
+    request: true,
+    response: true,
+    sign: true,
+    verify: true
+  },
+  {
+    language: 'Rust',
+    maintainer: 'Jun Kurihara',
+    repo: 'https://github.com/junkurihara/httpsig-rs',
+    request: true,
+    response: false,
+    sign: true,
+    verify: true
+  },
+  {
+    language: 'Ruby',
+    maintainer: 'Miguel Landaeta',
+    repo: 'https://github.com/nomadium/linzer',
+    request: true,
+    response: true,
+    sign: true,
+    verify: true
+  },
+  {
+    language: 'NodeJS',
+    maintainer: 'Misskey Project',
+    maintainerLink: 'https://misskey-hub.net/en/',
+    repo: 'https://github.com/misskey-dev/node-http-message-signatures',
+    request: true,
+    response: true,
     sign: true,
     verify: true
   },
